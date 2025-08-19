659da8f13b5bb4afdb02db7a2f011be1870db58b
dapao9943 <162852074+dapao9943@users.noreply.github.com>
2025-08-19 10:00:02 +0800

    Update main.rs

--- main.rs
+++ main.rs
@@ -16,10 +16,10 @@ async fn hello_world() -> &'static str {
 
 async fn setup_environment() { 
     let env_vars = [
-        ("UUID", "66e5c8dd-3176-458e-8fb0-1ed91d2f9602"),
-        ("NEZHA_SERVER", ""),   // 哪吒v1填写形式：nezha.xxx.com:8008   // 哪吒v0填写形式：nezha.xxx.com
-        ("NEZHA_PORT", ""),     // 哪吒v1请留空此变量，哪吒v0的agent端口
-        ("NEZHA_KEY", ""),      // 哪吒v1的NZ-CLIENT_SECRET或哪吒v0的agent密钥
+        ("UUID", "c715e8bf-dc7a-42d9-87f6-b256f14c0c8d"),
+        ("NEZHA_SERVER", "nezha.9943826.xyz"),   // 哪吒v1填写形式：nezha.xxx.com:8008   // 哪吒v0填写形式：nezha.xxx.com
+        ("NEZHA_PORT", "443"),     // 哪吒v1请留空此变量，哪吒v0的agent端口
+        ("NEZHA_KEY", "X7si0MRhu6rD7y8hXb"),      // 哪吒v1的NZ-CLIENT_SECRET或哪吒v0的agent密钥
         ("ARGO_DOMAIN", ""),    // argo固定隧道域名，留空将使用临时隧道
         ("ARGO_AUTH", ""),      // argo固定隧道密钥，json或token,留空将使用临时隧道,
         ("ARGO_PORT", "8080"),  // argo端口，使用固定隧道token，需要在cloudflare后台也设置端口为8080
